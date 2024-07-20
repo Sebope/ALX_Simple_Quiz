019a1544474cbe8fca7dd3367dd7ce7358a2d33c
rosemary ledwaba <rosemaryledwaba@gmail.com>
2024-07-20 20:56:11 +0200

    complete quiz solution

---
+++ quiz.js
@@ -0,0 +1,13 @@
+function checkAnswer() {
+    // Function body
+    var correctAnswer = "4";
+    var userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+    if(userAnswer ===correctAnswer){
+        document.getElementById("feedback").textContent ="Correct! Well done.";
+
+    }else{
+        document.getElementById("feedback").textContent ="Thats incorrect.Try again!";
+
+    }
+}
+document.getElementById("submit-answer").addEventListener('click',checkAnswer)
